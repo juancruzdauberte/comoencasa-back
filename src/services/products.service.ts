ff34fb5ef6b0673bd7b67409a9ae233c11885658
juancruzdauberte <juandauberte@gmail.com>
2025-06-09 11:36:40 -0300

    optimizate query products

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -2,15 +2,31 @@ import { db } from "../db/db";
 
 export class ProductService {
   static async getProductsCategory() {
-    const [rows] = await db.query("SELECT * FROM categoria");
-    return rows;
+    const conn = await db.getConnection();
+    try {
+      const [rows] = await db.query("SELECT * FROM categoria");
+      return rows;
+    } catch (error) {
+      console.error(error);
+      await conn.rollback();
+    } finally {
+      conn.release();
+    }
   }
 
   static async getProductsByCategory(category: string) {
-    const [rows] = await db.query(
-      "SELECT producto.id, producto.nombre FROM producto INNER JOIN categoria ON categoria.id = producto.categoria_id WHERE categoria.nombre = ? ",
-      [category]
-    );
-    return rows;
+    const conn = await db.getConnection();
+    try {
+      const [rows] = await db.query(
+        "SELECT producto.id, producto.nombre FROM producto INNER JOIN categoria ON categoria.id = producto.categoria_id WHERE categoria.nombre = ? ",
+        [category]
+      );
+      return rows;
+    } catch (error) {
+      await conn.rollback();
+      console.error(error);
+    } finally {
+      conn.release();
+    }
   }
 }
